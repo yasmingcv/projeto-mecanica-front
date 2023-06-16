dea69a76497859c0bf27dd64803481a7679b2cb9
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2023-06-16 00:01:51 -0300

    AUtenticacao aluno

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 'use strict'
 
 import { autenticarAluno } from "./fetchvalidacao.js"
+
+const logarAluno = async (dadosAluno) => {
+    let verificarCredenciais = await autenticarAluno(dadosAluno)
+
+    if(verificarCredenciais.status == 200) {
+        window.location.href = '../pages/aluno/html/hall_aluno.html'
+    } else {
+        alert('Usuário não autenticado.')
+    }
+
+}
+
+// const aluno = {
+//     "email": "jainacinho@gmail.com",
+//     "senha": "senhaforte"
+// }
+
+const enviarEmailSenha = () => {
+    const email = document.getElementById('email').value
+    const senha = document.getElementById('senha').value
+
+    const aluno = {}
+    aluno.email = email
+    aluno.senha = senha
+
+    logarAluno(aluno)
+
+}
+
+const botaoEntrar = document.getElementById('entrar')
+botaoEntrar.addEventListener('click', enviarEmailSenha)
+
--- js/fetchvalidacao.js
+++ js/fetchvalidacao.js
@@ -4,23 +4,15 @@ export const autenticarAluno = async (aluno) => {
     const url = 'https://backend-usinagem.cyclic.app/v1/senai/usinagem/aluno/autenticar'
     const options = {
         method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+         },
         body: JSON.stringify(aluno)
     }
 
-    console.log(aluno);
-
     const response = await fetch(url, options)
-
-    console.log(response);
-
     const data = await response.json()
 
-    return data
+    return await data
 }
-
-const aluno = {
-    "email": "jainacinho@gmail.com",
-    "senha": "senhaforte"
-}
-
-console.log(await autenticarAluno(aluno))
