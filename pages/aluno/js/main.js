4bf79aa7adb93b19fbfaa5daef65068589004d38
Dani <danielalinodasilvaoliveira@gmail.com>
2023-06-19 16:04:07 -0300

    tela dos alunos em desenvolviento-falta o css

---
+++ pages/aluno/js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+//Importe do Nav Bar
+import'../../administrador/js/bars.js'
+
+//Aleta para o de saida caso o usuaria queria sair da aplicação
+//import'./alert.js'
+
+import '../../../js/app.js'
+
+
+//Importe para colocar imagens 
+import '../../administrador/js/cards.js'
